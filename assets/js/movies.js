ff7d0c5a0f60e7b22ccbe29fd3b79fed4bd477e3
nicolabricot <github@nicolabricot.com>
2013-08-15 02:19:33 +0200

    Design improvement

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,12 +1,5 @@
 $(function() {
 $('.tip').tooltip();
 $('a[rel="external"]').click(function() { window.open($(this).attr('href')); return false; });
-
-$('span.more-content').slideToggle(0); $('div.more-button').show(0); $('span.more-dots').show(0);
-$('div.more-button').click(function() {
-	$(this).parent().children('p').children('span').children('span.more-content').slideToggle();
-	$(this).parent().children('p').children('span').children('span.more-dots').toggle();
-	if($(this).text() == 'More') {$(this).text('Less');}
-	else {$(this).text('More');}
-});
+$('#go_home_you_are_drunk').click(function() { $('html,body').animate({scrollTop: 0}, 'slow'); return false; });
 });
